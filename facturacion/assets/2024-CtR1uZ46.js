e887454806b253b25e37f23fbfa784100d94388b
edwin-ortizp <edwin.ortizp@campusucc.edu.co>
2025-10-17 11:43:14 -0500

    Update to test

---
+++ facturacion/assets/2024-CtR1uZ46.js
@@ -0,0 +1,1 @@
+const a={"CELSIA S.A E.S.P.":{_metadata:{usuariosCount:null,esIncumbente:!1,tipoEmpresa:null}}};export{a as default};
---
+++ facturacion/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jikko-estimator/facturacion/sw.js', { scope: '/jikko-estimator/facturacion/' })})}
